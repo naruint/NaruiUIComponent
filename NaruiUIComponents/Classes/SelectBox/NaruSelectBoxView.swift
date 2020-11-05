aba6d7820897674785c6a9b32f02e9a20c0e899a
서창열 <kongbaguni@gmail.com>
2020-11-05 11:12:38 +0900

    selectBoxView IBInspectable 변수 추가,.

--- NaruiUIComponents/Classes/SelectBox/NaruSelectBoxView.swift
+++ NaruiUIComponents/Classes/SelectBox/NaruSelectBoxView.swift
@@ -9,7 +9,8 @@ import UIKit
 
 @IBDesignable
 public class NaruSelectBoxView: UIView {
-    
+    //MARK:-
+    //MARK:arrangeView
     override init(frame: CGRect) {
         super.init(frame: frame)
         arrangeView()
@@ -32,14 +33,18 @@ public class NaruSelectBoxView: UIView {
         view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
         for btn in buttons {
             btn.addTarget(self, action: #selector(self.onButtonSelectChange(_:)), for: .touchUpInside)
-            btn.setTitleColor(selectedColor, for: .selected)
-            btn.setTitleColor(normalColor, for: .normal)
+            btn.setTitleColor(selectedTColor, for: .selected)
+            btn.setTitleColor(normalTColor, for: .normal)
         }
         fixLayout()
     }
-    
+    //MARK:-
+    //MARK:IBOutlet
     
     @IBOutlet var buttons:[UIButton] = []
+    //MARK:-
+    //MARK:IBInspectable
+
     @IBInspectable var titles:String = "" {
         didSet {
             for (index,title) in titles.components(separatedBy: ",").enumerated() {
@@ -51,7 +56,7 @@ public class NaruSelectBoxView: UIView {
         }
     }
     
-    @IBInspectable var selectedColor:UIColor?  {
+    @IBInspectable var selectedTColor:UIColor?  {
         set {
             for btn in buttons {
                 btn.setTitleColor(newValue, for: .selected)
@@ -62,7 +67,7 @@ public class NaruSelectBoxView: UIView {
         }
     }
     
-    @IBInspectable var normalColor:UIColor? {
+    @IBInspectable var normalTColor:UIColor? {
         set {
             for btn in buttons {
                 btn.setTitleColor(newValue, for: .normal)
@@ -73,6 +78,14 @@ public class NaruSelectBoxView: UIView {
         }
     }
     
+    /** 선택했을 때 라인 컬러*/
+    @IBInspectable var selectedLColor:UIColor = .white
+    /** 미선택시 라인컬러 */
+    @IBInspectable var normalLColor:UIColor = .gray
+    
+    //MARK:-
+    //MARK:IBAction
+
     @IBAction func onTouchupButton(_ sender:UIButton) {
         if sender.isSelected {
             return
@@ -87,17 +100,17 @@ public class NaruSelectBoxView: UIView {
         }
     }
     
+    //MARK:-
+    //MARK:custom methods
+
     @objc func onButtonSelectChange(_ sender:UIButton) {
         fixLayout()
     }
     
     func fixLayout() {
-        guard let selectedColor = selectedColor , let normalColor = normalColor else {
-            return
-        }
         for btn in buttons {
             btn.layer.borderWidth = 1
-            btn.layer.borderColor = btn.isSelected ? selectedColor.cgColor : normalColor.cgColor
+            btn.layer.borderColor = btn.isSelected ? selectedLColor.cgColor : normalLColor.cgColor
         }
     }
     
