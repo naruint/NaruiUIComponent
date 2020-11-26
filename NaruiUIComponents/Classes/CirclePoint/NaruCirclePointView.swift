010d99d019c5e4a9a7ba4b10b8f71df35cd93ab7
서창열 <kongbaguni@gmail.com>
2020-11-26 11:12:44 +0900

    CirclePointView

---
+++ NaruiUIComponents/Classes/CirclePoint/NaruCirclePointView.swift
@@ -0,0 +1,54 @@
+//
+//  NaruCirclePointView.swift
+//  NaruiUIComponents
+//
+//  Created by Changyeol Seo on 2020/11/26.
+//
+
+import UIKit
+
+@IBDesignable
+public class NaruCirclePointView: UIView {
+
+    @IBInspectable var maxLevel:Int = 3
+    @IBInspectable var currentLevel:Int = 1
+    @IBInspectable var offColor:UIColor = .gray
+    @IBInspectable var onColor:UIColor = .red
+    @IBInspectable var circleSize:CGFloat = 7
+    @IBInspectable var padding:CGFloat = 4
+    
+    var colors:[UIColor] {
+        var result:[UIColor] = []
+        for i in 0..<maxLevel {
+            let alpha:CGFloat = 1.0 - CGFloat(i) * (1.0 / (CGFloat(maxLevel) * 2.0))
+            let newColor = onColor.cgColor.copy(alpha: alpha)!
+            result.append(UIColor(cgColor: newColor))
+        }
+        return result.reversed()
+    }
+    
+    public var level:Int {
+        set {
+            currentLevel = newValue
+        }
+        get {
+            return currentLevel
+        }
+    }
+    
+    public override func draw(_ rect: CGRect) {
+        print("maxLevel : \(maxLevel), currentLevel: \(currentLevel)")
+        for i in 0..<maxLevel {
+            let x = circleSize * CGFloat(i) + padding * CGFloat(i)
+            let shapeLayer = CAShapeLayer()
+            shapeLayer.path = UIBezierPath(ovalIn: CGRect(x: x, y: 0, width: circleSize, height: circleSize)).cgPath
+            if i > currentLevel {
+                shapeLayer.fillColor = offColor.cgColor
+            } else {
+                shapeLayer.fillColor = colors[i].cgColor
+            }
+            layer.addSublayer(shapeLayer)
+        }
+        
+    }
+}
