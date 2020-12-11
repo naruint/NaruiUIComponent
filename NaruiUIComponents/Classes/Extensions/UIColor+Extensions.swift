4f4c01d3c8b754ba80e7177f338fa8f760e9f323
서창열 <kongbaguni@gmail.com>
2020-12-11 21:58:27 +0900

    라운드 이미지 만들기 추가.

--- NaruiUIComponents/Classes/Extensions/UIColor+Extensions.swift
+++ NaruiUIComponents/Classes/Extensions/UIColor+Extensions.swift
@@ -52,6 +52,30 @@ public extension UIColor {
         let img = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         return img
-
+    }
+    
+    /** 라운드 이미지 만들기 (라운드 BG 버튼 만들기 용)*/
+    func roundImage(cornerRadius:CGFloat, size:CGSize, strockColor:UIColor? = nil, strockWidth:CGFloat = 0)->UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        let ctx = UIGraphicsGetCurrentContext()!
+        ctx.saveGState()
+     
+        let path = UIBezierPath(
+            roundedRect: CGRect(origin: CGPoint(x: strockWidth, y: strockWidth),
+                                size: CGSize(width: size.width - strockWidth * 2, height: size.height - strockWidth * 2)
+            ), cornerRadius: cornerRadius)
+        
+        self.setFill()
+        path.fill()
+        if let color = strockColor {
+            color.setStroke()
+            path.lineWidth = strockWidth
+            path.stroke()
+        }
+        ctx.addPath(path.cgPath)
+        ctx.restoreGState()
+        let img = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return img.resizableImage(withCapInsets: UIEdgeInsets(top: cornerRadius, left: cornerRadius, bottom: cornerRadius, right: cornerRadius), resizingMode: .stretch)
     }
 }
