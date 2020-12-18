6409f4ea1d5dd1941ceb6a98fda01b93ed51fac5
서창열 <kongbaguni@gmail.com>
2020-12-18 14:26:40 +0900

    다시그리기 관련 이슈 수정..

--- NaruiUIComponents/Classes/CirclePoint/NaruDotLineView.swift
+++ NaruiUIComponents/Classes/CirclePoint/NaruDotLineView.swift
@@ -16,28 +16,40 @@ class NaruDotLineView: UIView {
     @IBInspectable var dotSize:CGFloat = 2
     @IBInspectable var isHorizontal:Bool = false
     @IBInspectable var dotColor:UIColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    
     override func draw(_ rect: CGRect) {
-        func drawDot(color:UIColor) {
-            var max = Int((bounds.height - paddingTop - paddingBottom) / dashDistance )
-            if isHorizontal {
-                max = Int((bounds.width - paddingLeft - paddingRight) / dashDistance )
-            }
-            for i in 0...max  {
-                let shapeLayer = CAShapeLayer()
-                var x:CGFloat = CGFloat(bounds.midX - dotSize / 2)
-                var y:CGFloat = CGFloat(i) * dashDistance + paddingTop
-                if isHorizontal {
-                    x = CGFloat(i) * dashDistance + paddingLeft
-                    y = CGFloat(bounds.midY - 1)
-                }
-
-                shapeLayer.path = UIBezierPath(ovalIn: CGRect(x: x - dotSize / 2, y: y - dotSize / 2, width: dotSize, height: dotSize)).cgPath
-                shapeLayer.fillColor = color.cgColor
-                layer.addSublayer(shapeLayer)
-            }
-        }
-        drawDot(color: dotColor)
+        drawDot()
         backgroundColor = .clear
+    }
+    
+    func drawDot() {
+        
+        for slayer in layer.sublayers ?? [] {
+            slayer.removeFromSuperlayer()
+        }
+        
+        var max = Int((bounds.height - paddingTop - paddingBottom) / dashDistance )
+        if isHorizontal {
+            max = Int((bounds.width - paddingLeft - paddingRight) / dashDistance )
+        }
+        for i in 0...max  {
+            let shapeLayer = CAShapeLayer()
+            var x:CGFloat = CGFloat(bounds.midX - dotSize / 2)
+            var y:CGFloat = CGFloat(i) * dashDistance + paddingTop
+            if isHorizontal {
+                x = CGFloat(i) * dashDistance + paddingLeft
+                y = CGFloat(bounds.midY - 1)
+            }
+
+            shapeLayer.path = UIBezierPath(ovalIn: CGRect(x: x - dotSize / 2, y: y - dotSize / 2, width: dotSize, height: dotSize)).cgPath
+            shapeLayer.fillColor = dotColor.cgColor
+            layer.addSublayer(shapeLayer)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        drawDot()
     }
 
 }
