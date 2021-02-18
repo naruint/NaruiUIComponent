04deda2666d4473aa6fb8778582085273d1a5a31
서창열 <kongbaguni@gmail.com>
2021-02-18 14:05:44 +0900

    난이도 표시 뷰 컬러 세팅하기 관련 메서드 수정.

--- NaruiUIComponents/Classes/CirclePoint/NaruCirclePointView.swift
+++ NaruiUIComponents/Classes/CirclePoint/NaruCirclePointView.swift
@@ -11,21 +11,15 @@ import UIKit
 /** 동그라미 로 표시하는 난이도*/
 public class NaruCirclePointView: UIView {
 
-    @IBInspectable var maxLevel:Int = 3
     @IBInspectable var currentLevel:Int = 1
-    @IBInspectable var offColor:UIColor = .gray
-    @IBInspectable var onColor:UIColor = .red
     @IBInspectable var circleSize:CGFloat = 7
     @IBInspectable var padding:CGFloat = 4
 
-    var colors:[UIColor] {
-        var result:[UIColor] = []
-        for i in 0..<maxLevel {
-            let alpha:CGFloat = 1.0 - CGFloat(i) * (1.0 / (CGFloat(maxLevel) * 2.0))
-            let newColor = onColor.cgColor.copy(alpha: alpha)!
-            result.append(UIColor(cgColor: newColor))
-        }
-        return result.reversed()
+    public var onColors:[UIColor] = [.red,.green,.blue]
+    public var offColors:[UIColor] = [.gray,.gray,.gray]
+    
+    var maxLevel:Int {
+        onColors.count
     }
     
     public var level:Int {
@@ -44,9 +38,9 @@ public class NaruCirclePointView: UIView {
             let shapeLayer = CAShapeLayer()
             shapeLayer.path = UIBezierPath(ovalIn: CGRect(x: x, y: y, width: circleSize, height: circleSize)).cgPath
             if i > currentLevel {
-                shapeLayer.fillColor = offColor.cgColor
+                shapeLayer.fillColor = offColors[i].cgColor
             } else {
-                shapeLayer.fillColor = colors[i].cgColor
+                shapeLayer.fillColor = onColors[i].cgColor
             }
             layer.addSublayer(shapeLayer)
         }
